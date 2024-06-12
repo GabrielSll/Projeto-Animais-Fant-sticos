4a084a93a8f80008b32c1347d0b286ae3c735249
Neo Oficina <gabrielsilvaaa369@gmail.com>
2024-06-12 17:18:50 -0300

    seleção de elementos 2

---
+++ script.js
@@ -0,0 +1,10 @@
+const animais = document.getElementById('animais');
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+console.log(gridSection)
+
+const primeiraUl = document.querySelector('ul');
+const primeiraLi = document.querySelector('li');
+const linksInternos = document.querySelector('href^=#;')
+
