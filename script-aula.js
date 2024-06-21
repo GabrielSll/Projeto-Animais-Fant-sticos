17ae5c520d24a35b8d217b51db6121a0eb62e74d
Neo Oficina <gabrielsilvaaa369@gmail.com>
2024-06-21 16:43:55 -0300

    Javascript finalizado

--- script-aula.js
+++ script-aula.js
@@ -477,3 +477,25 @@ const faq = document.querySelector('.faq');
 const cloneH1 = novoH1.cloneNode(true) 
 
 faq.appendChild(cloneH1)
+
+// Navegação por listas
+
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo');
+        });
+        tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index)
+        })
+    })
+}   
